bba6e1ee270e124c51a6c96026d6f2a9737a824a
Tree-Nulbo <exopas58@gmail.com>
2019-04-15 01:06:09 +0900

    Prepared to use expo
    Unlike previously, I'm going to use IOS application for making twitter-system interface.
    I have set up everything to use react-native and expo

---
+++ Twitter-System/src/components/Welcome.js
@@ -0,0 +1,25 @@
+import React from "react";
+import styled from "styled-components/native";
+
+const Root = styled.View`
+  alignitems: center;
+  justifycontent: center;
+  flex: 1;
+  backgroundcolor: ${props => props.theme.WHITE};
+  width: 90%;
+  alignself: center;
+`;
+
+const Text = styled.Text`
+  color: ${props => props.theme.PRIMARY};
+  fontsize: 18;
+  textalign: center;
+`;
+
+export default function Welcome() {
+  return (
+    <Root>
+      <Text>Welcome, if you see this that mean everything work!!!</Text>
+    </Root>
+  );
+}
