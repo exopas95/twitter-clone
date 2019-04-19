31cf9d5b8674f66973c2f19176a9be7b9b7da667
Tree-Nulbo <exopas58@gmail.com>
2019-04-19 19:42:12 +0900

    Applied apollo 2.0 to react-native
    FINALLY DONE!! ALL ERROR FIXED!!

--- server/Twitter_UI/App.js
+++ server/Twitter_UI/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { UIManager } from "react-native";
+import { ApolloProvider } from "react-apollo";
+import { ThemeProvider } from "styled-components";
+
+import { store, client } from "./src/store";
+import { colors } from "./src/utils/constants";
+
+import Welcome from "./src/components/Welcome";
+
+if (UIManager.setLayoutAnimationEnabledExperimental) {
+  UIManager.setLayoutAnimationEnabledExperimental(true);
+}
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <ApolloProvider store={store} client={client}>
+        <ThemeProvider theme={colors}>
+          <Welcome />
+        </ThemeProvider>
+      </ApolloProvider>
     );
   }
 }
@@ -14,8 +28,8 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
