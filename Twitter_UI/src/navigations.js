f11ea75ac695f627c338de3da5b2d11912ffddec
Tree-Nulbo <exopas58@gmail.com>
2019-04-29 06:08:25 +0900

    Navigator created
    Top and Bottom navigator created

--- Twitter_UI/src/navigations.js
+++ Twitter_UI/src/navigations.js
@@ -1,19 +1,112 @@
 import React, { Component } from "react";
 import {
+  addNavigationHelpers,
   StackNavigator,
-  createAppContainer,
-  createTabNavigator
+  TabNavigator
 } from "react-navigation";
 import { connect } from "react-redux";
+import { FontAwesome } from "@expo/vector-icons";
 
 import HomeScreen from "./Screens/HomeScreen";
+import ExploreScreen from "./Screens/ExploreScreen";
+import NotificationsScreen from "./Screens/NotificationsScreen";
+import ProfileScreen from "./Screens/ProfileScreen";
 
-const AppMainNav = StackNavigator({
-  Home: {
-    screen: HomeScreen
+import { colors } from "./utils/constants";
+
+const TAB_ICON_SIZE = 20;
+
+const Tabs = TabNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+      navigationOptions: () => ({
+        headerTitle: "Home",
+        tabBarIcon: ({ tintColor }) => (
+          <FontAwesome size={TAB_ICON_SIZE} color={tintColor} name="home" />
+        )
+      })
+    },
+    Explore: {
+      screen: ExploreScreen,
+      navigationOptions: () => ({
+        headerTitle: "Explore",
+        tabBarIcon: ({ tintColor }) => (
+          <FontAwesome size={TAB_ICON_SIZE} color={tintColor} name="search" />
+        )
+      })
+    },
+    Notifications: {
+      screen: NotificationsScreen,
+      navigationOptions: () => ({
+        headerTitle: "Notifications",
+        tabBarIcon: ({ tintColor }) => (
+          <FontAwesome size={TAB_ICON_SIZE} color={tintColor} name="bell" />
+        )
+      })
+    },
+    Profile: {
+      screen: ProfileScreen,
+      navigationOptions: () => ({
+        headerTitle: "Profile",
+        tabBarIcon: ({ tintColor }) => (
+          <FontAwesome size={TAB_ICON_SIZE} color={tintColor} name="user" />
+        )
+      })
+    }
+  },
+  {
+    lazy: true,
+    tabBarPosition: "bottom",
+    swipeEnabled: false,
+    tabBarOptions: {
+      showIcon: true,
+      showLabel: false,
+      activeTintColor: colors.PRIMARY,
+      inactiveTintColor: colors.LIGHT_GRAY,
+      style: {
+        backgroundColor: colors.WHITE,
+        height: 50,
+        paddingVertical: 5
+      }
+    }
   }
-});
+);
 
+const AppMainNav = StackNavigator(
+  {
+    Home: {
+      screen: Tabs
+    }
+  },
+  {
+    cardStyle: {
+      backgroundColor: "#F1F6FA"
+    },
+    navigationOptions: () => ({
+      headerStyle: {
+        backgroundColor: colors.WHITE
+      },
+      headerTitleStyle: {
+        fontWeight: "bold",
+        color: colors.SECONDARY,
+        alignSelf: "center"
+      }
+    })
+  }
+);
+
+class AppNavigator extends Component {
+  render() {
+    const nav = addNavigationHelpers({
+      dispatch: this.props.dispatch,
+      state: this.props.nav
+    });
+    return <AppMainNav navigation={nav} />;
+  }
+}
+
+/*
 class AppNavigator extends Component {
   render() {
     const nav = {
@@ -23,7 +116,7 @@ class AppNavigator extends Component {
     return <AppMainNav navigation={nav} />;
   }
 }
-
+*/
 export default connect(state => ({
   nav: state.nav
 }))(AppNavigator);
